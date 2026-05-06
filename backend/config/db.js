be1ad965a3267f69db49618dc054ba2c7f423127
cv-sukanya <sukanya@chronicleventures.com>
2026-05-06 18:31:59 +0530

    updated mysql

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,28 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 60000
 });
 
-connection.connect((err) => {
+// Test DB connection
+db.getConnection((err, connection) => {
   if (err) {
-    console.log("DB Connection Failed", err);
+    console.log("DB Connection Failed:", err);
   } else {
     console.log("MySQL Connected");
+
+    connection.release();
   }
 });
 
-module.exports = connection;
+module.exports = db;
